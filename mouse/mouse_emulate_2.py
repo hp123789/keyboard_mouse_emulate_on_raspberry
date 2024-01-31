821030e86e13623369f44605df18d50b62333091
hp123789 <45138135+hp123789@users.noreply.github.com>
2024-01-31 13:26:06 -0800

    Update mouse_emulate_2.py

--- mouse/mouse_emulate_2.py
+++ mouse/mouse_emulate_2.py
@@ -20,8 +20,8 @@ class MouseClient():
 		self.state = [0, 0, 0, 0]
 		self.bus = dbus.SystemBus()
 		self.btkservice = self.bus.get_object(
-			'org.npl.btkbservice', '/org/npl/btkbservice')
-		self.iface = dbus.Interface(self.btkservice, 'org.npl.btkbservice')
+			'org.thanhle.btkbservice', '/org/thanhle/btkbservice')
+		self.iface = dbus.Interface(self.btkservice, 'org.thanhle.btkbservice')
 	def send_current(self):
 		try:
 			self.iface.send_mouse(0, bytes(self.state))
